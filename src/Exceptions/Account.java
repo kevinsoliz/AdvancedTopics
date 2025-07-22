4527ade365ac90357d944905a41e6574cd5bb781
Kevin Solz <kevinsoliz10@gmail.com>
2025-07-22 12:24:52 +0200

    Channing exceptions

--- src/Exceptions/Account.java
+++ src/Exceptions/Account.java
@@ -8,10 +8,17 @@ public class Account {
         if (value < 0)
             throw new IOException();
     }
-
-    public void withdraw(float value) throws InsufficientFundException {
-        if(value > balance)
-            throw new InsufficientFundException();
-
+    //Channing exceptions: wrapping an exception inside a more general exception. Se usa sobre todo para
+    // construir libraries and frameworks.
+    public void withdraw(float value) throws AccountException {
+        if(value > balance) {
+            //Hay una manera más sencilla de escribir todo este código (en la AccountException);
+            var fundsException = new InsufficientFundException();
+            var accountException = new AccountException();
+            accountException.initCause(fundsException);
+            throw accountException;
+        }
     }
+
+
 }
