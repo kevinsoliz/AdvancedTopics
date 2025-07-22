38cd7f09635abacf1502215dc98340204935da65
Kevin Solz <kevinsoliz10@gmail.com>
2025-07-22 17:11:02 +0200

    Generic types that work with primitive values: Boxing y unboxing

--- src/Generics/Main.java
+++ src/Generics/Main.java
@@ -2,7 +2,9 @@ package Generics;
 
 public class Main {
     public static void main(String[] args) {
-        var list = new GenericList<Object>();
+        var numbers = new GenericList<Integer>();
+        numbers.add(1); //Boxing la Java Compiler va a poner dentro de una caja.
+        int number = numbers.get(0); //Unboxing
 
     }
 }
