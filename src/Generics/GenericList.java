62c386e210d58ed4c05f8ce256d4a7895e2d6625
Kevin Solz <kevinsoliz10@gmail.com>
2025-07-22 22:00:53 +0200

    para ver el bytecode tienes que pinchar en la pestaña de la clase

--- src/Generics/GenericList.java
+++ src/Generics/GenericList.java
@@ -1,7 +1,8 @@
 package Generics;
 //la T representa el objeto que queremos guardar en la lista
 public class GenericList<T> { //this T is bounded, is restricted
-    private T[] items = (T[]) new Object[10];
+    private T[] items = (T[]) new Object[10]; //Type erasure: Java compiler erases this type parameters and
+                                                //replaces them with a class or a interface depending on the constraints.
     private int count;
 
     public void add(T item) {
