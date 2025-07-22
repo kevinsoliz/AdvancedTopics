15a9fd2ff8c54f5adca8044a9702c87cd5859800
Kevin Solz <kevinsoliz10@gmail.com>
2025-07-22 11:45:26 +0200

    Podemos lanzar checked exceptions para validar datos que vienen desde fuera, del usuario o sistemas externos

---
+++ src/Exceptions/ExceptionsDemo.java
@@ -0,0 +1,15 @@
+package Exceptions;
+
+import java.io.IOException;
+
+public class ExceptionsDemo {
+    public static void show(){
+        var account = new Account();
+        try {
+            account.deposit(1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
